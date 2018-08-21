db196b395eeb7a320b32d9e69ae2997644bcc48b
KlintQinami <kq2129@columbia.edu>
2018-08-21 16:18:23 +0000

    Initial Commit

--- supercond_test.py
+++ supercond_test.py
@@ -5,8 +5,14 @@ import pyqtgraph as pg
 import visa
 import subprocess
 import pyqtgraph.exporters
+import time
+import csv
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
 def get_devices():
+    '''Try finding experiment devices and returning them'''
     subprocess.check_call("gpib_config")
     rm = visa.ResourceManager('@py')
     instruments = rm.list_resources()
@@ -30,14 +36,19 @@ def get_devices():
             continue
     return lakeshore, keithley, voltmeter
 
-def temp_follow_f(lakeshore, keithley, voltmeter):
-    return
 
-def superconductivity_ul(tempFollow, tParam, sampleCurr, dvmRange, ptInterval):
-    numDeltaPoints = 25
-    deltaDelay = 1
-    lakeshore, keithley, voltmeter = get_devices() 
-    print "SOUR:DELT:NVPR: " + keithley.query('SOUR:DELT:NVPR?')
+def check_devices(lakeshore, keithley):
+    if (not lakeshore or not keithley):
+        print "Error: Missing GPIB Device"
+        return -1
+    is2182aConnected = keithley.query('SOUR:DELT:NVPR?')
+    if int(is2182aConnected) != 1:
+        print "Keithley 2182 Voltmeter is not connected"
+        return -1
+    return 0
+
+
+def arm_keithley(keithley, dvmRange, sampleCurr, deltaDelay, numDeltaPoints):
     keithley.write('SYST:COMM:SER:SEND "VOLT:RANGE ' + str(dvmRange) + '"')
     keithley.write('*RST')
     keithley.write('SOUR:DELT:HIGH ' + str(sampleCurr))
@@ -45,13 +56,188 @@ def superconductivity_ul(tempFollow, tParam, sampleCurr, dvmRange, ptInterval):
     keithley.write('SOUR:DELT:COUN ' + str(numDeltaPoints))
     keithley.write('TRAC:CLE')
     keithley.write('SOUR:DELT:ARM')
-    print "KRDG?: " + lakeshore.query('KRDG?')
-    print "KRDG?a: " + lakeshore.query('KRDG?a')
-    print "SRDG?a: " + lakeshore.query('SRDG?a')
-    print "KRDG?b: " + lakeshore.query('KRDG?b')
+    time.sleep(10)
+    print "Keithley 6220 is armed (locked and loaded)"
+    return
+
+
+def disarm_keithley(keithley):
+    keithley.write('SOUR:SWE:ABOR')
+    keithley.write('TRAC:CLE')
+    print "Keithley 6220 is disarmed"
+    return
+
+
+def temp_follow_loop(lakeshore, keithley, sampleCurr, numDeltaPoints, 
+        deltaDelay):
+    newTempl = float(lakeshore.query('KRDG?a'))
+    newDiode = float(lakeshore.query('SRDG?a'))
+    newTempu = float(lakeshore.query('KRDG?b'))
     keithley.write('INIT:IMM')
-    print "Keith TRAC:DATA?: " + keithley.query('TRAC:DATA?')
-    print "Volt TRAC:DATA?: " + voltmeter.query('TRAC:DATA?')
+    time.sleep(numDeltaPoints * deltaDelay + 0.1)
+    keithley.write('TRAC:DATA?')
+    time.sleep(numDeltaPoints * deltaDelay + 0.1)
+    keithley.read()
+    keithley.write('CALC2:FORM MEAN')
+    keithley.write('CALC2:STAT ON')
+    keithley.write('CALC2:IMM')
+    keithley.write('CALC2:DATA?')
+    avgVoltage = float(keithley.read())
+    avgOhms = avgVoltage / sampleCurr
+    return [newTempu, newTempl, newDiode, sampleCurr, avgVoltage, avgOhms]
+
+
+def temp_follow_f(lakeshore, keithley, tParam, sampleCurr,
+        ptInterval, numDeltaPoints, deltaDelay):
+    '''Collect data until the temperature tParam is reached'''
+    newTempl = float(lakeshore.query('KRDG?a'))
+    tempSign = np.sign(newTempl - tParam)
+    newTempSign = tempSign
+    data = []
+    while tempSign == newTempSign:
+        newData = temp_follow_loop(lakeshore, keithley, sampleCurr, 
+                numDeltaPoints, deltaDelay)
+        data.append(newData)
+        time.sleep(ptInterval)
+        newTempSign = np.sign(newData[1] - tParam)
+    return data
+
+
+def temp_follow_a(lakeshore, keithley, tParam, sampleCurr, ptInterval, 
+        numDeltaPoints, deltaDelay):
+    '''Collect tParam number of data points'''
+    data = []
+    for i in range(tParam):
+        newData = temp_follow_loop(lakeshore, keithley, sampleCurr, 
+                numDeltaPoints, deltaDelay)
+        data.append(newData)
+        time.sleep(ptInterval)
+    return data
+
+
+def temp_follow(lakeshore, keithley, tempFollow, tParam, sampleCurr, dvmRange, 
+        ptInterval, numDeltaPoints, deltaDelay):
+    if (tempFollow == "f"):
+        return temp_follow_f(lakeshore, keithley, tParam, sampleCurr, 
+                ptInterval, numDeltaPoints, deltaDelay)
+    elif (tempFollow == "a"):
+        return temp_follow_a(lakeshore, keithley, tParam, sampleCurr, 
+                ptInterval, numDeltaPoints, deltaDelay)
+    return
+
+
+def superconductivity_ul(tempFollow, tParam, sampleCurr, dvmRange, ptInterval,
+        numDeltaPoints, deltaDelay):
+    lakeshore, keithley, voltmeter = get_devices() 
+    if (check_devices(lakeshore, keithley)):
+        return -1
+    arm_keithley(keithley, dvmRange, sampleCurr, deltaDelay, numDeltaPoints)
+    data = temp_follow(lakeshore, keithley, tempFollow, tParam, sampleCurr, 
+            dvmRange, ptInterval, numDeltaPoints, deltaDelay)
+    disarm_keithley(keithley)
+    keithley.close()
+    lakeshore.close()
+    return data
+
+
+def write_data(filename, data):
+    with open(filename, "wb") as outfile:
+        writer = csv.writer(outfile, delimiter=',')
+        for line in data:
+            writer.writerow(line)
+    return
+
+class AppForm(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        self.create_main()
+        self.setGeometry(100, 100, 800, 800)
+        self.setWindowTitle("Superconductivity Lab")
+        return
+
+    def create_btns(self):
+        self.save_btn = QPushButton("Save data", self)
+        self.start_btn = QPushButton("Start", self)
+        self.stop_btn = QPushButton("Stop", self)
+        self.set_parameters_btn = QPushButton("Set parameters", self)
+        self.save_btn.clicked.connect(self.save_data)
+        self.start_btn.clicked.connect(self.start_exp)
+        self.stop_btn.clicked.connect(self.stop_exp)
+        self.set_parameters_btn.clicked.connect(self.set_params)
+        self.save_btn.clicked.connect(self.save_data)
+        self.save_btn.resize(self.save_btn.minimumSizeHint())
+        return
+
+    def create_lines(self):
+        self.temp_follow_input = QLineEdit()
+        self.t_param_input = QLineEdit()
+        self.sample_curr_input = QLineEdit()
+        self.dvm_range_input = QLineEdit()
+        self.pt_interval_input = QLineEdit()
+        self.num_delta_points_input = QLineEdit()
+        self.delta_delay_input = QLineEdit()
+        return
+
+    def add_lines(self):
+        self.rows = QFormLayout()
+        self.rows.addRow("Temperature follow (a or f): ", 
+                self.temp_follow_input)
+        self.rows.addRow("T param (eg. 1): ", self.t_param_input)
+        self.rows.addRow("Sample current [amps] (eg. 0.01): ", 
+                self.sample_curr_input)
+        self.rows.addRow("DVM range (eg. 1): ", self.dvm_range_input)
+        self.rows.addRow("PT interval (eg. 1): ", self.pt_interval_input)
+        self.rows.addRow("Number of delta points (eg. 10): ", 
+                self.num_delta_points_input)
+        self.rows.addRow("Delta delay (eg. 0.1): ", self.delta_delay_input)
+        self.rows.addRow(self.set_parameters_btn)
+        return
+
+    def create_main(self):
+        page = QWidget()
+        self.create_btns()
+        self.create_lines()
+        self.setCentralWidget(page)
+        self.plot = pg.PlotWidget()
+        self.add_lines()
+        vbox = QGridLayout()
+        vbox.addLayout(self.rows, 0, 0, 1, 3)
+        vbox.addWidget(self.plot)
+        vbox.addWidget(self.start_btn)
+        vbox.addWidget(self.stop_btn)
+        vbox.addWidget(self.save_btn)
+        page.setLayout(vbox)
+        return
+   
+    def set_params(self):
+        return
+
+    def start_exp(self):
+        return
+
+    def stop_exp(self):
+        return
+
+    def save_data(self):
+        return
+
 
 if __name__ == "__main__":
-    superconductivity_ul("f", 1, 0.01, 1, 1)
+    app = QApplication(sys.argv)
+    form = AppForm()
+    form.show()
+    app.exec_()
+
+'''
+    tempFollow = "a"
+    tParam = 3
+    sampleCurr = 0.01
+    dvmRange = 1
+    ptInterval = 1
+    numDeltaPoints = 25
+    deltaDelay = 0.1
+    filename = "superconductivity_data.txt"
+    data = superconductivity_ul(tempFollow, tParam, sampleCurr, dvmRange, 
+            ptInterval, numDeltaPoints, deltaDelay)
+    write_data(filename, data) '''
+ 
