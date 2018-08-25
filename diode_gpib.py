65b5fb7389c8d384bd5e25055e875c88790ca51e
Klint Qinami <kq2129@columbia.edu>
2018-08-25 13:35:16 -0400

    Diode_gpib done, awaiting testing

--- diode_gpib.py
+++ diode_gpib.py
@@ -24,6 +24,9 @@ class AppForm(QMainWindow):
         self.create_main()
         self.setGeometry(100, 100, 800, 800)
         self.setWindowTitle("Superconductivity Diode Lab")
+        self.keithley = None
+        self.voltmeter = None
+        self.lakeshore = None
         try:
             self.get_devices()
         except Exception:
@@ -48,14 +51,16 @@ class AppForm(QMainWindow):
         self.curr_l_input = QLineEdit()
         self.curr_h_input = QLineEdit()
         self.curr_inc_input = QLineEdit()
+        self.num_meas_input = QLineEdit()
         return
 
 
     def add_lines(self):
         self.rows = QFormLayout()
-        self.rows.addRow("Current High: ", self.curr_h_input)
-        self.rows.addRow("Current Low: ", self.curr_l_input)
-        self.rows.addRow("Current Increment: ", self.curr_inc_input)
+        self.rows.addRow("Current High [amps]: ", self.curr_h_input)
+        self.rows.addRow("Current Low [amps]: ", self.curr_l_input)
+        self.rows.addRow("Current Increment [amps]: ", self.curr_inc_input)
+        self.rows.addRow("# Measurements per point: ", self.num_meas_input)
         self.rows.addRow(self.set_parameters_btn)
         return
 
@@ -65,14 +70,17 @@ class AppForm(QMainWindow):
             curr_h = float(self.curr_h_input.text())
             curr_l = float(self.curr_l_input.text())
             curr_inc = float(self.curr_inc_input.text())
+            num_meas = int(self.num_meas_input.text())
             self.curr_h = curr_h
             self.curr_l = curr_l
             self.curr_inc = curr_inc
+            self.num_meas = num_meas
             success_message = \
                 "Parameters set to: \n" + \
                 "Current High: " + str(self.curr_h) + "\n" + \
                 "Current Low: " + str(self.curr_l) + "\n" + \
-                "Current Increment: " + str(self.curr_inc)
+                "Current Increment: " + str(self.curr_inc) + "\n" + \
+                "# Measurements per point " + str(self.num_meas)
             QMessageBox.about(self, "Success", success_message)
         except Exception as inst:
             QMessageBox.about(self, 'Error', str(inst.args))
@@ -112,47 +120,6 @@ class AppForm(QMainWindow):
         return
    
 
-    def diode_measurement(self):
-        new_templ = float(self.lakeshore.query('KRDG?a'))
-        new_diode = float(self.lakeshore.query('SRDG?a'))
-        new_tempu = float(self.lakeshore.query('KRDG?b'))
-        self.keithley.write('INIT:IMM')
-        time.sleep(self.num_delta_points * self.delta_delay + 0.1)
-        self.keithley.write('TRAC:DATA?')
-        time.sleep(self.num_delta_points * self.delta_delay + 0.1)
-        self.keithley.read()
-        self.keithley.write('CALC2:FORM MEAN')
-        self.keithley.write('CALC2:STAT ON')
-        self.keithley.write('CALC2:IMM')
-        self.keithley.write('CALC2:DATA?')
-        avg_voltage = float(self.keithley.read())
-        avg_ohms = abs(avg_voltage / self.sample_curr)
-        self.data.append([new_templ, new_tempu, new_diode, self.sample_curr, 
-            avg_voltage, avg_ohms])
-        self.update_plot()
-        time.sleep(self.pt_interval)
-        return
-
-        
-    def update_plot(self):
-        self.x[self.graph_index] = self.data[-1][1]
-        self.y[self.graph_index] = self.data[-1][-1]
-        self.graph_index += 1
-        if self.graph_index >= self.x.shape[0]:
-            tmp = self.x
-            tms = self.y
-            self.x = np.empty(self.x.shape[0] * 2)
-            self.x[:tmp.shape[0]] = tmp
-            self.y = np.empty(self.y.shape[0] * 2)
-            self.y[:tms.shape[0]] = tms
-        self.curve.setData(self.x[:self.graph_index], 
-        self.y[:self.graph_index])
-
-        print self.data[-1]
-        app.processEvents()
-        return
-
-
     def initialize_keithleys(self):
         compliance = 105
         self.keithley.write('*RST')
@@ -167,8 +134,46 @@ class AppForm(QMainWindow):
         return
 
 
+    def diode_measurement(self):
+        curr_r = self.curr_l
+        self.keithley.write('OUTP ON')
+        while curr_r < self.curr_h + self.curr_inc:
+            self.keithley.write('SOUR:CURR ' + str(curr_r))
+            v_sum = 0
+            tl_sum = 0
+            tu_sum = 0
+            for i in range(self.num_meas):
+                tl_sum += float(self.lakeshore.query('KRDG?a'))
+                tu_sum += float(self.lakeshore.query('KRDG?b'))
+                time.sleep(0.1)
+                v_sum += float(self.voltmeter.query('READ?'))
+            self.data.append([v_sum/self.num_meas, curr_r, tl_sum, tu_sum])
+            self.update_plot()
+            curr_r += self.curr_inc
+        return
+
+        
+    def update_plot(self):
+        self.x[self.graph_index] = self.data[-1][0]
+        self.y[self.graph_index] = self.data[-1][1]
+        self.graph_index += 1
+        if self.graph_index >= self.x.shape[0]:
+            tmp = self.x
+            tms = self.y
+            self.x = np.empty(self.x.shape[0] * 2)
+            self.x[:tmp.shape[0]] = tmp
+            self.y = np.empty(self.y.shape[0] * 2)
+            self.y[:tms.shape[0]] = tms
+        self.curve.setData(self.x[:self.graph_index], 
+        self.y[:self.graph_index])
+
+        print self.data[-1]
+        app.processEvents()
+        return
+    
+
     def start_exp(self):
-        if (not self.keithley or not self.lakeshore):
+        if (not self.keithley or not self.lakeshore or not self.voltmeter):
             try:
                 self.get_devices()
                 if (self.check_devices()):
@@ -179,19 +184,22 @@ class AppForm(QMainWindow):
                 QMessageBox.about(self, 'Error', "Instruments not found")
                 return
         self.stop = False
-        self.arm_keithley()
-        self.temp_follow_m()
+        self.initialize_keithleys()
+        self.diode_measurement()
         return
 
 
     def stop_exp(self):
-        if (self.keithley and self.lakeshore):
+        if (self.keithley):
             self.stop = True
-            self.disarm_keithley()
+            self.keithley.write('OUTP OFF')
+            self.keithley.write('SOUR:CLE')
             self.keithley.close()
             self.lakeshore.close()
+            self.voltmeter.close()
             self.keithley = None
             self.lakeshore = None
+            self.voltmeter = None
         return
 
 
@@ -199,8 +207,7 @@ class AppForm(QMainWindow):
         try:
             outfile = QFileDialog.getSaveFileName(self, "Save File")
             with open(outfile, "wb") as f:
-                f.write("new_tempu,new_templ,new_diode,sample_curr," + \
-                        "avg_voltage,avg_ohms\n")
+                f.write("vav,curr_r,tl_sum,tu_sum\n")
                 writer = csv.writer(f, delimiter=',')
                 for line in self.data:
                     writer.writerow(line)
@@ -211,9 +218,9 @@ class AppForm(QMainWindow):
 
 
     def set_plot(self):
-        self.plot.setTitle("Resistance vs Temperature")
+        self.plot.setTitle("Resistance vs Voltage")
         self.plot.setLabel('left', 'Resistance', units='ohms')
-        self.plot.setLabel('bottom', 'Temperature', units='kelvin')
+        self.plot.setLabel('bottom', 'Voltage', units='volts')
         self.curve = self.plot.plot(pen="b", symbol='o', symbolPen=None, 
                 symbolSize=5, symbolBrush=(255,255,255,255))
         self.plot.setDownsampling(mode='peak')
